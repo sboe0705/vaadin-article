51630482f9e4ee4df1fa9fb1dfbf0d25381039ca
Sebastian Böhm <mail@sebastian-boehm.de>
2014-10-26 21:22:33 +0100

    created jsf pages

---
+++ jsf-business-application/src/de/gebit/businessapplication/jsf/Address.java
@@ -0,0 +1,45 @@
+package de.gebit.businessapplication.jsf;
+
+public class Address {
+	
+	private String street;
+	
+	private String city;
+	
+	private String zipcode;
+	
+	private String country;
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
---
+++ jsf-business-application/src/de/gebit/businessapplication/jsf/AddressbookBean.java
@@ -0,0 +1,12 @@
+package de.gebit.businessapplication.jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressbookBean {
+	
+	private List<Address> addresses = new ArrayList<>();
+	
+	private Address newAdress;
+
+}
