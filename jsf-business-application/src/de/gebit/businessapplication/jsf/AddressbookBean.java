4d007d5d9e8b6300592be11fdc0e196175c9d9ca
Sebastian Böhm <mail@sebastian-boehm.de>
2014-10-30 21:32:09 +0100

    simplified managed bean registration

--- jsf-business-application/src/de/gebit/businessapplication/jsf/AddressbookBean.java
+++ jsf-business-application/src/de/gebit/businessapplication/jsf/AddressbookBean.java
@@ -3,6 +3,11 @@ package de.gebit.businessapplication.jsf;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "addressbookBean")
+@SessionScoped
 public class AddressbookBean {
 
 	private final List<Address> addresses = new ArrayList<>();
