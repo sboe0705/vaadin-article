347a75f8c51e97d038bdb68fcfd7b43c4e3767f0
Sebastian Böhm <mail@sebastian-boehm.de>
2014-10-30 06:48:07 +0100

    completed vaadin business application

---
+++ vaadin-business-application/src/de/gebit/businessapplication/vaadin/Address.java
@@ -0,0 +1,45 @@
+package de.gebit.businessapplication.vaadin;
+
+public class Address {
+	
+	private String street;
+	
+	private String zipcode;
+	
+	private String city;
+	
+	private String country;
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
--- vaadin-business-application/src/de/gebit/businessapplication/vaadin/VaadinBusinessApplicationUI.java
+++ vaadin-business-application/src/de/gebit/businessapplication/vaadin/VaadinBusinessApplicationUI.java
@@ -1,14 +1,23 @@
 package de.gebit.businessapplication.vaadin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -21,19 +30,45 @@ public class VaadinBusinessApplicationUI extends UI {
 	public static class Servlet extends VaadinServlet {
 	}
 
+	private BeanFieldGroup<Address> addressFieldGroup = new BeanFieldGroup<>(Address.class);
+
+	private BeanItemContainer<Address> addressContainer = new BeanItemContainer<>(Address.class);
+
 	@Override
 	protected void init(VaadinRequest request) {
-		final VerticalLayout layout = new VerticalLayout();
-		layout.setMargin(true);
-		setContent(layout);
+		TextField streetField = new TextField("Straße");
+		streetField.setNullRepresentation("");
+		TextField zipcodeField = new TextField("PLZ");
+		zipcodeField.setNullRepresentation("");
+		TextField cityField = new TextField("Stadt");
+		cityField.setNullRepresentation("");
+		TextField countryField = new TextField("Land");
+		countryField.setNullRepresentation("");
 
-		Button button = new Button("Click Me");
-		button.addClickListener(new Button.ClickListener() {
-			public void buttonClick(ClickEvent event) {
-				layout.addComponent(new Label("Thank you for clicking"));
-			}
-		});
-		layout.addComponent(button);
+		addressFieldGroup.bind(streetField, "street");
+		addressFieldGroup.bind(zipcodeField, "zipcode");
+		addressFieldGroup.bind(cityField, "city");
+		addressFieldGroup.bind(countryField, "country");
+		addressFieldGroup.setItemDataSource(new Address());
+
+		FormLayout formLayout = new FormLayout(streetField, zipcodeField, cityField, countryField);
+
+		Button saveButton = new Button("Speichern", event -> save());
+
+		Table table = new Table();
+		table.setContainerDataSource(addressContainer);
+
+		setContent(new VerticalLayout(formLayout, saveButton, table));
+	}
+
+	public void save() {
+		try {
+			addressFieldGroup.commit();
+			addressContainer.addBean(addressFieldGroup.getItemDataSource().getBean());
+			addressFieldGroup.setItemDataSource(new Address());
+		} catch (CommitException e) {
+			Notification.show(e.getMessage(), Type.ERROR_MESSAGE);
+		}
 	}
 
 }
